30fb12a74bbcddec41cfffcf06a55c9c78366041
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-14 17:27:50 +0000

    Finish ASCII art initials using mulitline string.

---
+++ app.py
@@ -0,0 +1,21 @@
+# Block Letters
+# ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.
+
+# Write a Python program called initials.py that displays the initials of your name in block letters as shown and dip your toes into ASCII art.
+
+# Szymon W
+# I was a diving legend on Koh Tao.
+
+name = """
+ SSSSS     W                   W
+S     S     W                 W
+S            W               W
+ SSSSS        W      W      W
+      S        W    W W    W
+S     S         W  W   W  W
+ SSSSS           W       W
+
+ """ ;
+
+
+print(name);
